0ddd7ac203036104d8a9c00da052c942d214d9df
Brent Perry <burentoperii@gmail.com>
2016-12-10 15:48:14 -0600

    add function to reverse first and last letters.

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,11 @@ var toEncode = prompt("Enter a sentence: ");
 var capitalize = function (sentence) {
   var wordLength = sentence.length;
   return sentence.charAt(0).toUpperCase() + sentence.slice(1, wordLength - 1) + sentence.charAt(wordLength - 1).toUpperCase();
-  };
+};
 
-alert(capitalize(toEncode));
+var reverseFirstLast = function (sentence) {
+  var wordLength = sentence.length;
+  return sentence.charAt(wordLength - 1) + sentence.slice(1, wordLength - 1) + sentence.charAt(0);
+}
+
+alert(reverseFirstLast(capitalize(toEncode)));
